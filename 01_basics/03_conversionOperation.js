215d8243487ebbe73797d229a8732d1b23bb83e8
Pratiksha Sunil Sable <pratikshasable95@gmail.com>
2025-01-17 12:22:10 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,5 +19,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber =24
 let stringNumber =Object(someNumber)
-console.log(typeof(stringNumber))
+//console.log(typeof(stringNumber))
 //Conversion of datatyape
+
+//*************Operations **************/
+let a =2
+let b=3
+//console.log(a+b)
+
+let value = 3
+let negValue= -value;
+//console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2/2)
+//console.log(2*2)
+//console.log(2%2)
+//console.log(2**2)
+
+let str1="Hello"
+let str2 ="pratik"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log(1 + 2 + "2")
+// console.log(true)
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter)
+
