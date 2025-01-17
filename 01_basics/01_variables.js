ee80d23b6bd69a6b793de80c018891975fee0c0c
Pratiksha Sunil Sable <pratikshasable95@gmail.com>
2025-01-17 09:39:10 +0000

    Push ConatineDatatype config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 12346;
+let acoountEmail = "pratiksha@gmail.com"
+var accountPassword ="123467"
+let accountState;
+accountCity ="Pune"
+
+//accountID =2 //not allowed
+acoountEmail ="hs@sd.com"
+accountPassword ="64467374"
+accountCity = "us"
+/*
+prefre not use to var
+beacuase of issue in block ascope and functional scope
+*/
+console.log(accountID);
+console.table([accountID, acoountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello");// we are using node js, not browser
+
+console.log(3+4);
+console.log("pratiksha"); //https://tc39.es/
+
+let name="Pratiksha" //string
+let age =30; //number
+let isLoggedIn = false //boolean
+let state;
+
+//number =>2 to power 53
+//bigInt
+//string=> ""
+//boolean => true/false
+//null ==> standalone value
+//undefined
+//symbol =>> unique
+
+//above are primitive datatyeps
+
+// object
+
+console.log(typeof "Pratiksha")
+console.log(typeof undefined) //undefined
+console.log(typeof undefined) // object
