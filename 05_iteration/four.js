40511d1548c1524b539cef3c2a4e1b281d19cf50
Pratiksha Sunil Sable <pratikshasable95@gmail.com>
2025-01-29 13:30:30 +0000

    Forin loop

--- 05_iteration/four.js
+++ 05_iteration/four.js
@@ -1,5 +1,15 @@
+//Object are iterated by using map
 const myObject= {
     game1: 'NFS',
     game2: 'Conuter',
     game3: 'spiderman'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ':-', value);
+// }
+
+for (const key in myObject) {
+   console.log(`${key} shortcut is for ${myObject[key]}`);
+   
 }
